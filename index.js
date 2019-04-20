73e93baa41448c9d416f0bba74b77f1d442efa93
Scott Bickell <bickellworks@gmail.com>
2019-04-19 18:04:40 -0600

    Fix typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function createNode(element) {
 
 function append(parent, el) {
     return parent.appendChild(el);
-    
+
 }
 
 const ul = document.getElementById('authors');
@@ -17,7 +17,7 @@ fetch(url)
         return authors.map(function(author) {
             let li = createNode('li'),
                 img = createNode('img'),
-                span = creteNode('span');
+                span = createNode('span');
             img.src = author.picture.medium;
             span.innerHTML= `${author.name.first} ${author.name.last}`;
             append(li, img);
